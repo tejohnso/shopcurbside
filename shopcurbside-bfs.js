16d8dc69d191870c78ef4d0eaf5f6b934a0337af
Tyler Johnson <mail@tyler-johnson.ca>
2016-06-02 08:36:51 -0400

    Refactor - Move function defs out of recursion

--- shopcurbside-bfs.js
+++ shopcurbside-bfs.js
@@ -6,37 +6,36 @@ function bfs(nodes) {
 
   return fetchNode(nodes.shift())
   .then(outputMessage)
-  .then(concatNewNodes)
+  .then(concatNewNodes.bind(null, nodes))
   .then(bfs.bind(null))
   .catch((err)=>{console.log(err);});
-
-
-  function fetchNode(node) {
-    return refreshSession()
-    .then((session)=>{
-      return fetch("http://challenge.shopcurbside.com/" + node, {headers: {"Session": session}});
-    })
-    .then((resp)=>{
-      return resp.json();
-    });
-  }
-
-  function outputMessage(json) {
-    if (json.error) {throw new Error(json.error);}
-    if (json.message) {console.log(json.message);}
-    if (json.secret) {console.log(json.secret);}
-    return json;
-  }
-
-  function concatNewNodes(json) {
-    json.next = json.Next || json.nExt || json.neXt || json.nexT || json.next;
-    if (json.next) {
-      if (!Array.isArray(json.next)) {json.next = [json.next];}
-      nodes = nodes.concat(json.next);
-    }
-
-    return nodes;
-  }
 }
 
 bfs(["start"]);
+
+function fetchNode(node) {
+  return refreshSession()
+  .then((session)=>{
+    return fetch("http://challenge.shopcurbside.com/" + node, {headers: {"Session": session}});
+  })
+  .then((resp)=>{
+    return resp.json();
+  });
+}
+
+function outputMessage(json) {
+  if (json.error) {throw new Error(json.error);}
+  if (json.message) {console.log(json.message);}
+  if (json.secret) {console.log(json.secret);}
+  return json;
+}
+
+function concatNewNodes(nodes, json) {
+  json.next = json.Next || json.nExt || json.neXt || json.nexT || json.next;
+  if (json.next) {
+    if (!Array.isArray(json.next)) {json.next = [json.next];}
+    nodes = nodes.concat(json.next);
+  }
+
+  return nodes;
+}
