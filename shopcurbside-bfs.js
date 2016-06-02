093671feb8b7bd8aa751cab9f2efad11f8604ec8
Tyler Johnson <mail@tyler-johnson.ca>
2016-06-02 08:23:13 -0400

    Refactor - clarify processing steps

--- shopcurbside-bfs.js
+++ shopcurbside-bfs.js
@@ -4,25 +4,39 @@ refreshSession = require("./session-refresh.js");
 function bfs(nodes) {
   if (nodes.length === 0) {return;}
 
-  return refreshSession()
-  .then((session)=>{
-    return fetch("http://challenge.shopcurbside.com/" + nodes.shift(), {headers: {"Session": session}});
-  })
-  .then((resp)=>{
-    return resp.json();
-  })
-  .then((json)=>{
+  return fetchNode(nodes.shift())
+  .then(outputMessage)
+  .then(concatNewNodes)
+  .then(bfs.bind(null))
+  .catch((err)=>{console.log(err);});
+
+
+  function fetchNode(node) {
+    return refreshSession()
+    .then((session)=>{
+      return fetch("http://challenge.shopcurbside.com/" + node, {headers: {"Session": session}});
+    })
+    .then((resp)=>{
+      return resp.json();
+    });
+  }
+
+  function outputMessage(json) {
     if (json.error) {throw new Error(json.error);}
     if (json.message) {console.log(json.message);}
     if (json.secret) {console.log(json.secret);}
+    return json;
+  }
+
+  function concatNewNodes(json) {
     json.next = json.Next || json.nExt || json.neXt || json.nexT || json.next;
     if (json.next) {
       if (!Array.isArray(json.next)) {json.next = [json.next];}
       nodes = nodes.concat(json.next);
     }
-    return bfs(nodes);
-  })
-  .catch((err)=>{console.log(err);});
+
+    return nodes;
+  }
 }
 
 bfs(["start"]);
